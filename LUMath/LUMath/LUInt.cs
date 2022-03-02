4e787e7df3e4654b374fce07e17b644df9f11c2a
LU <1150463081@qq.com>
2022-03-02 17:05:24 +0800

    Update：首次提交

---
+++ LUMath/LUMath/LUInt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LUMath
+{
+    public struct LUInt
+    {
+        private long scaledValue;
+        public long ScaledValue
+        {
+            get { return scaledValue; }
+            set { scaledValue = value; }
+        }
+
+        const int BIT_MOVE_COUNT = 10;
+        const int MUTLIPLIER_FACTOR = 1 << 10;
+
+        public LUInt(int value)
+        {
+            scaledValue = value * MUTLIPLIER_FACTOR;
+        }
+        public LUInt(float value)
+        {
+            scaledValue = (long)Math.Round(value * MUTLIPLIER_FACTOR);
+        }
+    }
+}
---
+++ LUMath/TestExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LUMath;
+
+namespace TestExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LUInt num_1 = new LUInt(5);
+            LUInt num_2 = new LUInt(0.5f);
+            Console.WriteLine(num_1.ScaledValue);
+            Console.WriteLine(num_2.ScaledValue);
+            Console.ReadKey();
+        }
+    }
+}
